cc56954b01d186662beeea2dc1270441ff0845e9
movsic08 <lejero08@gmail.com>
2023-08-02 13:24:10 +0800

    Fixed Layout, FOR REVISION

--- resources/views/components/main_nav.blade.php
+++ resources/views/components/main_nav.blade.php
@@ -1,6 +1,7 @@
 <section
     class="md:gradient-bg-light sticky top-0 z-30 bg-slate-100 bg-opacity-70 backdrop-blur-md md:px-7 md:py-1 lg:px-7">
-    <div class="top-0 flex w-full items-center justify-between px-7 py-2 text-base font-semibold text-blue-950 md:px-2">
+    <div
+        class="container top-0 flex w-full items-center justify-between px-7 py-2 text-base font-semibold text-blue-950 md:px-2">
         <h1 class="">Good Morning, Pogi!</h1>
         <div class="flex flex-row items-center justify-center">
             <h1>21-AC-0138</h1>
