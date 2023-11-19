8115fe6a8f9df4601c9c082472a3dfcc14df0e9e
Riemyl <johnriemyl11@gmail.com>
2023-11-19 22:46:05 +0800

    added loading spinner on pdf area

--- resources/views/livewire/components/pdf-viewer-security.blade.php
+++ resources/views/livewire/components/pdf-viewer-security.blade.php
@@ -83,6 +83,13 @@
 
                 <div class="col-span-12 mt-2 overflow-auto text-center" id="scrollableCanvas">
                     <div class="h-full w-auto">
+                        <div class="flex items-center justify-center h-full absolute top-0 left-0 w-full" id="loadingSpinner">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#6563ff" class="animate-spin h-10 w-10">
+                                <path
+                                    d="M12,7a1,1,0,1,0,1,1A1,1,0,0,0,12,7ZM6.30383,16.134a1,1,0,1,0,1.366.366A1,1,0,0,0,6.30383,16.134Zm11.39234,0a1,1,0,1,0,.366,1.366A.99992.99992,0,0,0,17.69617,16.134Zm2.91418-2.7912v-.00048a4.91843,4.91843,0,0,0-4.52588-1.19678L15.665,11.3833a4.98871,4.98871,0,0,0-2.50342-8.25146,5.03485,5.03485,0,0,0-4.27832.958A4.97824,4.97824,0,0,0,7,8a4.92853,4.92853,0,0,0,1.35156,3.3916l-.41845.75a4.9889,4.9889,0,0,0-5.92627,6.28613,5.02991,5.02991,0,0,0,2.96972,3.22608,4.96944,4.96944,0,0,0,6.58887-3.18994l.8667.01416a4.98119,4.98119,0,0,0,4.76025,3.52441,5.01726,5.01726,0,0,0,4.79981-3.57373A4.95006,4.95006,0,0,0,20.61035,13.34277Zm-.52881,4.49512a3.0059,3.0059,0,0,1-4.38574,1.75977,2.96456,2.96456,0,0,1-1.35156-1.70508,1.99436,1.99436,0,0,0-1.90967-1.42871h-.86914a1.99451,1.99451,0,0,0-1.90967,1.42822A2.98084,2.98084,0,0,1,5.70654,19.792a2.99293,2.99293,0,0,1,1.76709-5.7041,1.96659,1.96659,0,0,0,2.17383-.94238l.43555-.75342a1.99513,1.99513,0,0,0-.28125-2.36865,2.9795,2.9795,0,0,1,.3291-4.37012A2.99279,2.99279,0,0,1,14.19971,10.022a2.00015,2.00015,0,0,0-.28272,2.37012l.43506.75293a1.97369,1.97369,0,0,0,2.17432.94336,2.98771,2.98771,0,0,1,3.55517,3.74951Z"
+                                    fill="#6563ff" />
+                            </svg>
+                        </div>
                         <canvas class="mx-auto" id="pdfArea" style="height: auto; width: 100%;"></canvas>
                     </div>
                 </div>
@@ -102,10 +109,43 @@
             input[type=number] {
                 -moz-appearance: textfield;
             }
+
+            #loadingSpinner {
+                background-color: rgba(255, 255, 255, 0.8); /* Adjust opacity if needed */
+                z-index: 1000; /* Make sure it's above other elements */
+            }
+
         </style>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.min.js"></script>
         <script src="{{ asset('js/main.js') }}"></script>
+        <script>
+            // Add this script to handle PDF loading and remove the spinner
+            document.addEventListener('DOMContentLoaded', function () {
+                const loadingSpinner = document.getElementById('loadingSpinner');
+                const pdfArea = document.getElementById('pdfArea');
+
+                // Function to remove the loading spinner
+                function removeSpinner() {
+                    loadingSpinner.style.display = 'none';
+                }
+
+                // PDF.js code to load the PDF
+                pdfjsLib.getDocument('{{ asset('storage/' . decrypt($this->pdfFile)) }}').promise.then(function (pdfDoc) {
+                    const totalPages = pdfDoc.numPages;
+                    document.getElementById('totalPages').textContent = totalPages;
+
+                    // Your existing PDF rendering code here
+
+                    // After rendering the PDF, remove the spinner
+                    removeSpinner();
+                }).catch(function (error) {
+                    console.error('Error loading PDF:', error);
+                    // Handle error if needed
+                    removeSpinner(); // Make sure to remove the spinner even if there's an error
+                });
+            });
+        </script>
         <script>
             $(document).ready(function() {
                 // Set initial zoom level and limits
