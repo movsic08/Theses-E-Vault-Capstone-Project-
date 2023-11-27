61ef11b1b040fb0afd1de7f99f55b04d5b3a40cf
Riemyl <johnriemyl11@gmail.com>
2023-11-27 20:17:21 +0800

    fix some bug

--- public/js/copyClipboard.js
+++ public/js/copyClipboard.js
@@ -34,12 +34,13 @@ function setupClipboardCopy() {
             document.body.removeChild(textArea);
         }
     } else {
-        // console.error("One or more required elements not found.");
+        console.error("One or more required elements not found.");
     }
 }
 
 // Add an event listener for Livewire navigation
 document.addEventListener("livewire:navigated", () => {
+    // console.log("Livewire navigation");
     // Call the setup function when the document is ready
     setupClipboardCopy();
     // Re-initialize the Flowbite library or any other logic needed after navigation
