2ba68a8d54a916f2d6a2b7721d6dbf6d0978d8d7
movsic08 <lejero08@gmail.com>
2023-11-28 13:29:54 +0800

    patch on help

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
     {{-- @livewire('profile-edit-tab') --}}
     <div
-        class="relative grid flex-grow grid-flow-row-dense grid-cols-7 gap-4 px-7 py-2 text-primary-color md:mx-8 md:my-2 md:px-0">
+        class="relative grid flex-grow grid-flow-row-dense grid-cols-7 gap-4 overflow-hidden px-7 py-2 text-primary-color md:mx-8 md:my-2 md:px-0">
         <!-- Row 1, Column 1 -->
         <div class="z-10 col-span-7 flex flex-col justify-center md:col-span-7 lg:col-span-4">
             <h1 class="text-6xl font-black md:text-7xl">THESIS <span class="text-secondary-color">KIOSK</span></h1>
@@ -34,7 +34,8 @@
         <!-- Row 2, Column 2 -->
         <div
             class="z-10 col-span-7 flex items-center justify-center md:col-span-6 md:flex-row md:items-start md:justify-start lg:col-span-4">
-            <a href="{{ route('home', ['clicked' => true]) }}" class="animation w-full animate-bounce rounded-full bg-primary-color py-2 text-center text-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] shadow-blue-950 transition duration-1000 hover:bg-sky-900 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-sky-800 md:w-1/2">
+            <a href="{{ route('home', ['clicked' => true]) }}"
+                class="animation w-full animate-bounce rounded-full bg-primary-color py-2 text-center text-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] shadow-blue-950 transition duration-1000 hover:bg-sky-900 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-sky-800 md:w-1/2">
                 Explore now
             </a>
         </div>
--- resources/views/pages/user/help-and-support-user.blade.php
+++ resources/views/pages/user/help-and-support-user.blade.php
@@ -10,7 +10,7 @@
                     class="mx-[2rem] mt-7 text-center text-[1.13rem] font-extrabold text-primary-color dark:text-white md:text-2xl lg:mx-[10rem] lg:text-5xl">
                     How can we help you?</h2>
                 <p
-                    class="mx-[2rem] mt-4 w-full rounded-lg border border-blue-200 px-4 py-2 text-justify text-sm text-primary-color drop-shadow-md backdrop-blur-sm md:w-[85%] md:px-6 md:py-4 lg:w-[60%] lg:text-base">
+                    class="mx-[2rem] mt-4 w-full rounded-lg px-4 py-2 text-justify text-sm text-primary-color drop-shadow-md backdrop-blur-sm md:w-[85%] md:px-6 md:py-4 lg:w-[60%] lg:text-base">
                     Welcome to our
                     comprehensive Help and Support
                     Center for Theses
@@ -28,7 +28,7 @@
                     </div>
                     <div class="flex flex-col items-start justify-center gap-8 text-primary-color md:flex-row">
                         <div
-                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-slate-200 p-6 drop-shadow-lg">
+                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-white p-6 drop-shadow-lg duration-300 hover:bg-slate-200">
                             <img class="h-[6rem]" src="{{ asset('icons/logo.svg') }}" alt="logo">
                             <div class="mt-6">
                                 <strong class="text-lg uppercase"> Account</strong>
@@ -37,7 +37,7 @@
                             </div>
                         </div>
                         <div
-                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-slate-200 p-6 drop-shadow-lg">
+                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-white p-6 drop-shadow-lg duration-300 hover:bg-slate-200">
                             <img class="h-[6rem]" src="{{ asset('icons/logo.svg') }}" alt="logo">
                             <div class="mt-6">
                                 <strong class="text-lg uppercase"> Using Theses Kisok</strong>
@@ -46,7 +46,7 @@
                             </div>
                         </div>
                         <div
-                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-slate-200 p-6 drop-shadow-lg">
+                            class="flex h-[16rem] w-[16rem] flex-col items-center justify-center rounded-lg bg-white p-6 drop-shadow-lg duration-300 hover:bg-slate-200">
                             <img class="h-[6rem]" src="{{ asset('icons/logo.svg') }}" alt="logo">
                             <div class="mt-6">
                                 <strong class="text-lg uppercase">Reporting</strong>
